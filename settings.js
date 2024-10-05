3d60abcf8f1792785c399fd91ba6e30a94470d0b
wisdom uche <cartweird@icloud.com>
2024-10-05 13:41:38 +0100

    Update  ox settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "OxC4825536698e9367d81BddCFCc19044e9ad";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
